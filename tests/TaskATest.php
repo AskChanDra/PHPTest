3e4d420f701d58eb4650b3b22157415e0b9daa6c
AskChandra <cpthapa@gmail.com>
2020-11-28 19:39:26 +1100

    space

--- tests/TaskATest.php
+++ tests/TaskATest.php
@@ -12,48 +12,75 @@ use PHPTest\Task\TaskA;
 class TaskATest extends TestCase
 {
     public function testRandomHandOfFiveCardsCount() {
+
       $hand_array = TaskA::getFiveCards();
+
       $this->assertEquals( 5, count($hand_array));
+
     }
 
     public function testRandomHandHasValidRanks() {
+
       $ranks = $this->getRanks();
 
       $hand_array = TaskA::getFiveCards();
+
       $is_valid_rank = false;
+
       foreach($hand_array as $hand) {
+
         // 'a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k'
         $r = substr($hand, 0, -1);
+
         if(in_array($r, $ranks)){
+
           $is_valid_rank = true;
+
         }
         $this->assertTrue($is_valid_rank);
+
         $is_valid_rank = false;
+
       }
     }
 
     public function testRandomHandHasValidSuits() {
+
       $suits = $this->getSuits();
 
       $hand_array = TaskA::getFiveCards();
+
       $is_valid_suit = false;
+
       foreach($hand_array as $hand) {
+
         // 's', 'd', 'c', 'h'
         $s = substr($hand, -1);
+
         if(in_array($s, $suits)){
+
           $is_valid_suit = true;
+
         }
         $this->assertTrue($is_valid_suit);
+
         $is_valid_suit = false;
+
       }
+
     }
 
     private function getRanks() {
+
       return [ 'a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k' ];
+
     } 
     
     private function getSuits() {
+
       return [ 's', 'd', 'c', 'h' ];
+
     }
+    
 }
 
--- tests/TaskBTest.php
+++ tests/TaskBTest.php
@@ -10,22 +10,35 @@ use PHPUnit\Framework\TestCase;
 use PHPTest\Task\TaskB;
 
 class TaskBTest extends TestCase {
+
     public function testHandIsStraight() {
+
       $a_low  = ['10s', 'js', 'qc', 'ac', 'kc'];
+
       $straight = TaskB::is_straight($a_low);
+
       $this->assertTrue($straight);
 
       $a_high  = ['ad', '5d', '2d', '4d', '3d'];
+
       $straight = TaskB::is_straight($a_high);
+
       $this->assertTrue($straight);
+
     }
     public function testHandIsStraighFlush() {
+
       $a_low  = ['10s', 'js', 'qc', 'ac', 'kc'];
+
       $flush = TaskB::is_straight($a_low);
+
       $this->assertTrue($flush);
 
       $a_high  = ['ad', '5d', '2d', '4d', '3d'];
+
       $flush = TaskB::is_straight($a_high);
+      
       $this->assertTrue($flush);
+
     }
 }
